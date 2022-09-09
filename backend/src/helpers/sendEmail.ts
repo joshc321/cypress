20390747777dffd4e28674df8cf0c042aba5d4fb
Joshua Cordero <joshcordero2134@gmail.com>
2022-09-09 16:16:34 -0700

    updated sending email

--- backend/src/helpers/sendEmail.ts
+++ backend/src/helpers/sendEmail.ts
@@ -8,7 +8,7 @@ function sendEmail(to, subject, text, html)
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     const msg = {
     to: to, // Change to your recipient
-    from: 'joshcordero2134@gmail.com', // Change to your verified sender
+    from: 'cypress@joshcordero.com', // Change to your verified sender
     subject: subject,
     text: text,
     html: html,
